ce0d7f7511153d0fd8def4a3bdee7e5cddb80e11
Ivan Izdepskyi <johnystartup@gmail.com>
2019-01-16 16:46:02 +0200

    Spring programmatic transaction management

--- src/com/izdebski/dao/BankDAO.java
+++ src/com/izdebski/dao/BankDAO.java
@@ -1,10 +1,9 @@
 package com.izdebski.dao;
 
-import com.izdebski.exception.InsufficientAccountBalanceException;
 import com.izdebski.model.Account;
 
 public interface BankDAO {
 
-    public abstract void withdraw(Account fromAccount,Double withdrawAmt) throws InsufficientAccountBalanceException;
+    public abstract void withdraw(Account fromAccount,Double withdrawAmt);
     public abstract void deposit(Account toAccount,Double depositAmt);
 }
--- src/com/izdebski/model/Account.java
+++ src/com/izdebski/model/Account.java
@@ -2,39 +2,32 @@ package com.izdebski.model;
 
 public class Account {
 
-    private Long accountNumber;
+    private Integer accountNumber;
     private Double accountBalance;
     private String accounType;
     private String accountHolderName;
 
-    public Long getAccountNumber() {
+    public Integer getAccountNumber() {
         return accountNumber;
     }
-
-    public void setAccountNumber(Long accountNumber) {
+    public void setAccountNumber(Integer accountNumber) {
         this.accountNumber = accountNumber;
     }
-
     public Double getAccountBalance() {
         return accountBalance;
     }
-
     public void setAccountBalance(Double accountBalance) {
         this.accountBalance = accountBalance;
     }
-
     public String getAccounType() {
         return accounType;
     }
-
     public void setAccounType(String accounType) {
         this.accounType = accounType;
     }
-
     public String getAccountHolderName() {
         return accountHolderName;
     }
-
     public void setAccountHolderName(String accountHolderName) {
         this.accountHolderName = accountHolderName;
     }
